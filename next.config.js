84166821d5b60d396cd08803081d199c449d2e36
Emre Kelleci <emrek@4thwhale.com>
2021-06-30 20:58:38 -0400

    Index is added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+/* eslint-disable camelcase */
 // next.config.js
 module.exports = {
-    images: {
-      domains: ['cdn.shopify.com'],
-    },
-  }
+  images: {
+    domains: ["cdn.shopify.com"],
+  },
+
+  async rewrites() {
+    const rewrite_url = [
+        {
+            source: '/index.html',
+            destination: '/'
+        },
+    ]
+    return rewrite_url;
+}
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,17 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import React from "react";
-import { GetStaticProps } from 'next';
-import Image from 'next/image';
+import { GetStaticProps } from "next";
+import Image from "next/image";
 /**
  * Home: The Landing page of the web app
  * @return {JSX.Element} The JSX Code for the Home Page
  */
 
-type Props={
-  productsData:any;
-}
-export default function Home({productsData}:Props) {
+type Props = {
+  productsData: any;
+};
+export default function Index({ productsData }: Props) {
   console.log(productsData.products);
   return (
     <div className={styles.container}>
@@ -22,29 +22,30 @@ export default function Home({productsData}:Props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-      {
-        productsData.products.map((product,index)=>(
+        {productsData.products.map((product, index) => (
           <div key={index}>
-            <Image src={product.image.src} alt={product.title+'picture'} height={300} width={300} />
+            <Image
+              src={product.image.src}
+              alt={product.title + "picture"}
+              height={300}
+              width={300}
+            />
             <h1> {product.title}</h1>
           </div>
-        ))
-      }
+        ))}
       </div>
     </div>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-
-
   const productsApiHit = await fetch(process.env.NEXT_PUBLIC_SHOPIFY_LINK);
   const productsData = await productsApiHit.json();
 
   return {
-      props: {
-        productsData   
-      },
-      revalidate: 300 // Time in seconds before regenerating
+    props: {
+      productsData,
+    },
+    revalidate: 300, // Time in seconds before regenerating
   };
 };
